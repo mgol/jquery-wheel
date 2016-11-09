5484b538765a18244e0b61d46df3e0fb10d398ab
Michał Gołębiowski <m.goleb@gmail.com>
2016-11-09 13:19:04 +0100

    Update deps & the ESLint config, fix linting violations, use jQuery instead of $

--- src/jquery.wheel.js
+++ src/jquery.wheel.js
@@ -9,41 +9,41 @@
  * Thanks to Brandon Aaron (http://brandonaaron.net) for the idea of this plugin.
  */
 
-(function ($) {
+(function () {
     'use strict';
 
-    if ($.fn.wheel) { // already polyfilled
+    if (jQuery.fn.wheel) { // already polyfilled
         return;
     }
 
     // Normalizing event properties for the 'wheel' event (like event.which etc.).
     // This is only needed for jQuery <3.0.
-    if ($.event.fixHooks) {
-        $.event.fixHooks.wheel = $.event.mouseHooks;
+    if (jQuery.event.fixHooks) {
+        jQuery.event.fixHooks.wheel = jQuery.event.mouseHooks;
     }
 
     var handler = function (orgEvent) {
         var i;
         var args = Array(arguments.length);
-        var event = $.event.fix(orgEvent);
+        var ev = jQuery.event.fix(orgEvent);
 
         for (i = 0; i < args.length; i++) {
             args[i] = arguments[i];
         }
 
-        event.deltaMode = orgEvent.deltaMode;
-        event.deltaX = orgEvent.deltaX;
-        event.deltaY = orgEvent.deltaY;
-        event.deltaZ = orgEvent.deltaZ;
+        ev.deltaMode = orgEvent.deltaMode;
+        ev.deltaX = orgEvent.deltaX;
+        ev.deltaY = orgEvent.deltaY;
+        ev.deltaZ = orgEvent.deltaZ;
 
         // Exchange original event for the modified one in arguments list.
-        args[0] = event;
+        args[0] = ev;
 
-        return $.event.dispatch.apply(this, args);
+        return jQuery.event.dispatch.apply(this, args);
     };
 
     // Implementing jQuery `wheel` event via native `wheel` event.
-    $.event.special.wheel = {
+    jQuery.event.special.wheel = {
         setup: function () {
             this.addEventListener('wheel', handler);
         },
@@ -54,9 +54,9 @@
     };
 
     // Implement `$object.wheel()` and `$object.wheel(handler)`.
-    $.fn.wheel = function (data, fn) {
+    jQuery.fn.wheel = function (data, fn) {
         return arguments.length > 0 ?
             this.on('wheel', null, data, fn) :
             this.trigger('wheel');
     };
-})(jQuery);
+})();
